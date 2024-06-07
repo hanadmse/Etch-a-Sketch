7a7629f6a8caf26d3b89880f168712a1a42f3d24
hanadmse <hanadmse@gmail.com>
2024-06-06 22:13:34 -0500

    grid setup and color hovering

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
 const container = document.querySelector(".container");
 
 
-var rowDiv = document.createElement("div");
+
 let num = 16;
 for (i = 0; i < num; i++) {
-    row = document.createElement("div");
+    let row = document.createElement("div");
     row.className = "row";
     container.appendChild(row);
     for (j = 0; j < num; j++) {
-        square = document.createElement('div');
+        let square = document.createElement('div');
         square.className = "square";
         row.appendChild(square);
+        square.addEventListener("click", () => {
+            square.style.background = "red";
+        });
     }
 }
+
 
 
